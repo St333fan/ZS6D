4c4b3535e8b4d4049e4873cb6a514d384d92fca5
St333fan <st.lech10@gmail.com>
2024-03-29 15:08:08 +0100

    added CrocoClassExtractor

--- test_zs6d.py
+++ test_zs6d.py
@@ -1,15 +1,7 @@
 from zs6d import ZS6D
 import os
 import json
-
-# Loading the config file:
-with open(os.path.join("./zs6d_configs/bop_eval_configs/cfg_ycbv_inference_bop.json"), "r") as f:
-    config = json.load(f)
-
-# Instantiating the pose estimator:
-# This involves handing over the path to the templates_gt file and the corresponding object norm_factors.
-pose_estimator = ZS6D(config['templates_gt_path'], config['norm_factor_path'])
-
+from croco.models.croco import CroCoNet
 import cv2
 from PIL import Image
 import pose_utils.img_utils as img_utils
@@ -18,6 +10,20 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 import torch
+from croco.models.croco import CroCoNet
+import sys
+sys.path.append("croco")
+
+#ckpt = torch.load('/home/imw-mmi/PycharmProjects/ZS6D/pretrained_models/CroCo.pth')
+#model = CroCoNet(**ckpt.get('croco_kwargs', {}))
+
+# Loading the config file:
+with open(os.path.join("./zs6d_configs/bop_eval_configs/cfg_ycbv_inference_bop.json"), "r") as f:
+    config = json.load(f)
+
+# Instantiating the pose estimator:
+# This involves handing over the path to the templates_gt file and the corresponding object norm_factors.
+pose_estimator = ZS6D(config['templates_gt_path'], config['norm_factor_path'], model_type='croco')
 
 # Loading a ground truth file to access segmentation masks to test zs6d:
 with open(os.path.join(config['gt_path']), 'r') as f:
@@ -25,9 +31,6 @@ with open(os.path.join(config['gt_path']), 'r') as f:
 
 img_id = '000048_1'
 #img_id = '8'
-
-model = torch.load('/home/imw-mmi/PycharmProjects/ZS6D/pretrained_models/CroCo.pth')
-
 
 for i in range(len(data_gt[img_id])):
     obj_number = i
